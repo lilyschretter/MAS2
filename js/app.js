aead9730a5c99b475ed957a8d40cf9199c1d00ea
lilyschretter <104341899+lilyschretter@users.noreply.github.com>
2022-11-17 14:46:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,72 +38,75 @@ const coffees = [
   }
 ];
 const showCoffees = () => {
-  let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
+    let output = "";
+    coffees.forEach(
+        ({name, image}) =>
+            (output += `
               <div class="card">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
                 <a class="card--link" href="#">Taste</a>
               </div>
               `)
-  );
-  container.innerHTML = output;
+    );
+    container.innerHTML = output;
 };
 
 document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
-  function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
-  if (navigator.mediaDevices) {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }
-    
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-  if (legacyApi) {
-    // ...and promisify it
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err));
     });
-  }
+
 }
 
-function getStream (type) {
-  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
+function getUserMedia(constraints) {
+    // if Promise-based API is available, use it
+    if (navigator.mediaDevices) {
+        return navigator.mediaDevices.getUserMedia(constraints);
+    }
 
-  var constraints = {};
-  constraints[type] = true;
-  
-  getUserMedia(constraints)
-    .then(function (stream) {
-      var mediaControl = document.querySelector(type);
-      
-      if ('srcObject' in mediaControl) {
-        mediaControl.srcObject = stream;
-      } else if (navigator.mozGetUserMedia) {
-        mediaControl.mozSrcObject = stream;
-      } else {
-        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      }
-      
-      mediaControl.play();
-    })
-    .catch(function (err) {
-      alert('Error: ' + err);
-    });
-}}
+    // otherwise try falling back to old, possibly prefixed API...
+    var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    if (legacyApi) {
+        // ...and promisify it
+        return new Promise(function (resolve, reject) {
+            legacyApi.bind(navigator)(constraints, resolve, reject);
+        });
+    }
+}
+
+function getStream(type) {
+    if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+        !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+        alert('User Media API not supported.');
+        return;
+    }
+
+    var constraints = {};
+    constraints[type] = true;
+
+    getUserMedia(constraints)
+        .then(function (stream) {
+            var mediaControl = document.querySelector(type);
+
+            if ('srcObject' in mediaControl) {
+                mediaControl.srcObject = stream;
+            } else if (navigator.mozGetUserMedia) {
+                mediaControl.mozSrcObject = stream;
+            } else {
+                mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+            }
+
+            mediaControl.play();
+        })
+        .catch(function (err) {
+            alert('Error: ' + err);
+        });
+}
