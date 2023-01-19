7c4a34363335de92fe20720a2082919e50c8c8d3
lilyschretter <104341899+lilyschretter@users.noreply.github.com>
2023-01-19 16:14:07 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -410,4 +410,18 @@ if ('permissions' in navigator) {
     function requestMicrophone() {
         navigator.getUserMedia({audio: true}, noop, noop)
     }
+    document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline';
+
+var target = document.getElementById('target');
+
+function handleStateChange() {
+  var timeBadge = new Date().toTimeString().split(' ')[0];
+  var newState = document.createElement('p');
+  var state = navigator.onLine ? 'online' : 'offline';
+  newState.innerHTML = '' + timeBadge + ' Connection state changed to ' + state + '.';
+  target.appendChild(newState);
+}
+
+window.addEventListener('online', handleStateChange);
+window.addEventListener('offline', handleStateChange);
 }
